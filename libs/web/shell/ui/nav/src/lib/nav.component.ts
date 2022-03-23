e63344a46f438a148981194f4947e7a38807539f
nguyentu1996 <nguyentu19962710@gmail.com>
2022-03-23 13:08:11 +0700

    update

--- libs/web/shell/ui/nav/src/lib/nav.component.ts
+++ libs/web/shell/ui/nav/src/lib/nav.component.ts
@@ -1,5 +1,8 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-
+export interface DocProgram {
+  title: string;
+  href: string;
+}
 @Component({
   selector: 'web-shell-nav',
   styleUrls: ['nav.component.css'],
@@ -10,21 +13,63 @@ export class NavComponent implements OnInit {
   @ViewChild('sidenav', { static: true}) sidenav!: ElementRef<HTMLElement>;
   @ViewChild('sidenavContent', {static: true}) sidenavContent!: ElementRef<HTMLElement>;
   sideNavOffset = '300px';
-  lessions: string[] = [
-    'Introduction',
-    'What you should already know',
-    'JavaScript and Java',
-    'Hello world',
-    'Variables',
-    'Declaring variables',
-    'Variable scope',
-    'Global variables',
-    'Constants',
-    'Data types',
-    'if...else statement',
-    'while statement',
-    'Function declarations',
-    'Reference'
+  lessions: DocProgram[] = [
+    {
+      title: 'Introduction',
+      href: "#Introduction",
+    },
+    {
+      title: 'What you should already know',
+      href: "#What_you_should_already_know",
+    },
+    {
+      title: 'JavaScript and Java',
+      href: "#JavaScript_and_Java",
+    },
+    {
+      title: 'Hello world',
+      href: "#Hello_world",
+    },
+    {
+      title: 'Variables',
+      href: '#Variables'
+    },
+    {
+      title: 'Declaring variables',
+      href: '#Declaring_variables',
+    },
+    {
+      title: 'Variable scope',
+      href: '#Variable_scope',
+    },
+    {
+      title: 'Global variables',
+      href: '#Global_variables'
+    },
+    {
+      title: 'Constants',
+      href: '#Constants'
+    },
+    {
+      title: 'Data types',
+      href: '#Data_types,'
+    },
+    {
+      title: 'if...else statement',
+      href: '#If_else_statement'
+    },
+    {
+      title:  'while statement',
+      href: '#While_statement'
+    },
+    {
+      title: 'Function declarations',
+      href: '#Function_declarations'
+    },
+    {
+      title: 'Reference',
+      href: '#Reference'
+    }
   ];
 
   ngOnInit(): void {
